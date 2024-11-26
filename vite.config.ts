1942c957d9da54eb05806425014a744805299f3c
higashi <higashiyuga1@gmail.com>
2024-11-26 13:50:35 +0900

    発行

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import path from "path"; // ◀◀ 追加
+const repositoryName = "react-todo-app";
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === "production" ? `/${repositoryName}/` : "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        404: path.resolve(__dirname, "404.html"),
+      },
+    },
+  },
   server: {
     port: 3000, // デフォルトのポートを3000に設定
     strictPort: false,
